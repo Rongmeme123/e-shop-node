e216e43906ed4675a7d03237fa8db1b99055f958
Rongmeme123 <1025994116@qq.com>
2018-12-06 20:42:54 +0800

    push

--- app/config/cookie.js
+++ app/config/cookie.js
@@ -1,7 +1,7 @@
 module.exports = {
-    maxAge: 1000 * 3600 * 24 * 3,
-    // domain: 's19.ierg4210.ie.cuhk.edu.hk',
+    // maxAge: 1000 * 3600 * 24 * 3,
+    domain: 's19.ierg4210.ie.cuhk.edu.hk',
     domain: 'localhost',
     httpOnly: true,
-    secure: false,
+    secure: true,
 }
