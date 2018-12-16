50d3ba3618f09fc7630d242fc00b8dff91f02460
Rongmeme123 <1025994116@qq.com>
2018-12-16 11:52:15 +0800

    remove 1

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,6 @@ const router = require('koa-router')();
 const apiRoutes = require('../controllers/apis');
 const viewRoutes = require('../controllers/views');
 
-router.use('', async(ctx, next) => {
-    let url = ctx.request.url;
-    let prefix = /^\/api\//.test(url) ? 'api' : 'view';
-    console.log(`[${prefix} url] --- ${url}`);
-    await next(ctx)
-})
-
 router.use('/api', apiRoutes.routes(), apiRoutes.allowedMethods());
 router.use('', viewRoutes.routes());
 
